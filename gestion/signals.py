1e66ba89b7be97fe2dc17eec00aa2c48bf7d46bd
Strelixia <maguy.birikomo@gmail.com>
2025-01-20 08:40:55 +0100

    app debugging and style

--- gestion/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .models import Order
-from .utils import send_email
-
-@receiver(post_save, sender=Order)
-def handle_order_status_change(sender, instance, **kwargs):
-    if instance.status =='PAID':
-        send_email(instance, buyer_email='murairicedric@gmail.com', supplier_email ='maguy.birikomo@gmail.com')
